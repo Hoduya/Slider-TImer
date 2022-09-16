0675d8a9e677c6085ca71436ebab860143383dab
HoJun Choi <nyhj962@gmail.com>
2022-09-16 21:23:46 +0900

    Initial Commit

---
+++ FirstNewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstNewApp
+//
+//  Created by HoJun on 2022/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
